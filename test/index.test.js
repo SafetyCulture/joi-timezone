0a725b8b565f596143cef590fab3cb5f94a81ccc
jayco <jason.jacob@safetyculture.io>
2017-08-02 13:38:03 +1000

    basic timezone functionality

--- test/index.test.js
+++ test/index.test.js
@@ -7,8 +7,17 @@ const Joi = BaseJoi.extend(JoiTimezone);
 
 describe('string', () => {
   describe('Timezone()', () => {
+    it('return error for invalid timezone', () => {
+      const testValue = Joi.validate('Blah', Joi.string().timezone());
+      assert.isOk(testValue.error, 'should produce an error');
+      assert.isOk(testValue.error.name, 'should produce a ValidationError');
+    });
+
     it('validates strings with a valid timezone', () => {
-      assert.equal(true, false, 'should output the input value');
+      const testZone = 'Australia/Melbourne';
+      const testValue = Joi.validate(testZone, Joi.string().timezone());
+      assert.isNotOk(testValue.error, 'should not produce an error');
+      assert.equal(testValue.value, testZone);
     });
   });
 });
