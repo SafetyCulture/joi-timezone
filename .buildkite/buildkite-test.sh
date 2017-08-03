d6246665a3de4212e541b54aa0ce810801513d93
jayco <jason.jacob@safetyculture.io>
2017-08-03 11:55:04 +1000

    node 8

--- .buildkite/buildkite-test.sh
+++ .buildkite/buildkite-test.sh
@@ -3,4 +3,5 @@
 set -e
 
 n 4.7.2 && rm -rf ./node_modules && npm i && npm t && \
-n 6.9.4 && rm -rf ./node_modules && npm i && npm t
+n 6.9.4 && rm -rf ./node_modules && npm i && npm t && \
+n 8.2.1 && rm -rf ./node_modules && npm i && npm t
