bd9f09ba83bdfbccb13d85220c730a45494cbbed
Tristan Davey <tristan.davey@safetyculture.io>
2017-08-23 17:58:46 +1000

    CI upgrade: remove buildkite, add Travis + Coveralls & badges

--- .buildkite/buildkite-test.sh
+++
@@ -1,7 +0,0 @@
-# .buildkite/buildkite-test.sh
-
-set -e
-
-n 4.7.2 && rm -rf ./node_modules && npm i && npm t && \
-n 6.9.4 && rm -rf ./node_modules && npm i && npm t && \
-n 8.2.1 && rm -rf ./node_modules && npm i && npm t
